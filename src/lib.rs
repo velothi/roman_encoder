20cb43d478e2c2a8c265103b861c36bef2d9c8b5
Llathasa Veleth <llathasa@outlook.com>
2020-11-08 15:11:42 +0300

    Initial commit. v.1.0.0

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+mod roman {
+  pub trait Roman {
+    /// Encodes unicode string into roman numerals
+    /// # Examples
+    /// ```
+    /// println!("Hello, world! : {}", "Hello, world!".roman())
+    /// /* LXXII CI CVIII CVIII CXI XLIV XXXII CXIX CXI CXIV CVIII C XXXIII */
+    /// ```
+    fn roman(&self) -> String;
+    //TODO: unroman(&self) -> String
+  }
+
+  impl Roman for &str {
+    fn roman(&self) -> String {
+      let mut ord:               i64;
+      let mut min_ord:        String = String::new();
+      let mut encoded:   Vec<String> = Vec::new();
+      let alphabet: Vec<(&str, i64)> =
+          vec![("M", 1000), ("CM", 900),
+               ("D",  500), ("CD", 400),
+               ("C",  100), ("XC",  90),
+               ("L",   50), ("XL",  40),
+               ("X",   10), ("IX",   9),
+               ("V",    5), ("IV",   4),
+               ("I",    1)];
+      for c in self.chars() {
+        ord = c as i64;
+        for (symbol, value) in &alphabet {
+          while value <= &ord {
+            min_ord += symbol;
+            ord     -= value;
+          }
+        }
+        encoded.push(min_ord);
+        min_ord = String::new();
+      }
+      return encoded.join(" ");
+    }
+  }
+}
